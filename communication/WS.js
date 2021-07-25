e10aad72e3db5b97ea541ff5cca14939b2075196
Nihat <kaffinpx@gmail.com>
2021-07-25 17:22:45 +0300

    rate limit for ws api

--- communication/WS.js
+++ communication/WS.js
@@ -1,4 +1,6 @@
 const EventEmitter = require('events').EventEmitter
+
+const { RateLimiterMemory } = require('rate-limiter-flexible')
 const WebSocket = require('ws')
 
 module.exports = {
@@ -9,26 +11,36 @@ function serveWSApi (kafium, port) {
   const wss = new WebSocket.Server({ port: port })
   const wssEmitter = new EventEmitter
 
+  const rateLimiter = new RateLimiterMemory({
+    points: 5,
+    duration: 2,
+  })
+  
   wss.on('listening', function () {
     wssEmitter.emit('ready', port)
   })
 
   wss.on('connection', function (ws) {
-    ws.on('message', function (data) {
-      if (data.startsWith('getBlockByHash/')) {
-        ws.send(`Block/${kafium.getBlockByHash(data.split('/')[1]).toData()}`)
-      }
+    ws.on('message', async function (data) {
+      try {
+        await rateLimiter.consume(socket.handshake.address)
+        if (data.startsWith('getBlockByHash/')) {
+          ws.send(`Block/${kafium.getBlockByHash(data.split('/')[1]).toData()}`)
+        }
 
-      if (data.startsWith('getWalletBalance/')) {
-        ws.send(`walletBalance/${kafium.getBalanceOfAddress(data.split('/')[1])}`)
-      }
+        if (data.startsWith('getWalletBalance/')) {
+          ws.send(`walletBalance/${kafium.getBalanceOfAddress(data.split('/')[1])}`)
+        }
 
-      if (data.startsWith('getBlocksCount')) {
-        ws.send(`blocksCount/${kafium.getTotalBlocks()}`)
-      }
+        if (data.startsWith('getBlocksCount')) {
+          ws.send(`blocksCount/${kafium.getTotalBlocks()}`)
+        }
 
-      if (data.startsWith('getLastHash')) {
-        ws.send(`lastHash/${kafium.getLatestBlock().hash}`)
+        if (data.startsWith('getLastHash')) {
+          ws.send(`lastHash/${kafium.getLatestBlock().hash}`)
+        }
+      } catch (err) {
+        ws.send(`RATE_LIMIT`)
       }
     })
   })
